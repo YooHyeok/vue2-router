96bc444954d4166a5a05f8151b6ccc5bb966bbb2
webdevyoo <yjk7454@naver.com>
2024-08-05 00:00:21 +0900

    feat: `User.vue` - 컴포넌트 생성 및 route객체로 부터 파라미터 조회

---
+++ src/views/Users.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Users</h1>
+    <p>파라미터: {{ userId }}</p>
+    <p>파라미터: {{ $route.params.name }}</p>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    userId() {
+      return this.$route.params.userId
+    }
+  },
+  created() {
+    console.log('router', this.$router)
+    console.log('route', this.$route)
+  }
+}
+</script>
