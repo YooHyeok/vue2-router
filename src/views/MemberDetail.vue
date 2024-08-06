fc5856dc0872abe825729029fe2b456b159a177b
webdevyoo <yjk7454@naver.com>
2024-08-07 00:04:13 +0900

    feat: `MemerDetail.vue` - 부모 route로 부터 넘겨받은 params 출력 구현

---
+++ src/views/MemberDetail.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>{{ memberId }}회원 상세정보</h1>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    memberId() {
+      return this.$route.params.memberId
+    }
+  }
+}
+</script>
