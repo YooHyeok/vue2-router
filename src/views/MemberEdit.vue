0dd39b1ab991cfdf341567356ceeb29bec5142ef
webdevyoo <yjk7454@naver.com>
2024-08-08 00:01:49 +0900

    feat: `MemerEdit.vue` - 형제 route로 부터 넘겨받은 params 출력 구현

---
+++ src/views/MemberEdit.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>아이디 {{ memberId }} 회원 수정</h1>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    memberId() {
+      return this.$route.params.memberId
+    }
+  },
+}
+</script>
