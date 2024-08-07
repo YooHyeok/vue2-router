4874ee8d12535806f59d81c1f8c0424c002585cf
webdevyoo <yjk7454@naver.com>
2024-08-08 00:01:55 +0900

    feat: `MemberDetsil.vue` - 수정 버튼 추가 및 형제 객체인 MemberEdit.vue로 라우터 push

--- src/views/MemberDetail.vue
+++ src/views/MemberDetail.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <h1>{{ memberId }}회원 상세정보</h1>
+    <h1>아이디 {{ memberId }} 회원 상세정보</h1>
+    <v-btn @click="edit">수정</v-btn>
   </div>
 </template>
 <script>
@@ -8,6 +9,13 @@ export default {
   computed: {
     memberId() {
       return this.$route.params.memberId
+    }
+  },
+  methods: {
+    edit() {
+      // this.$router.push({name: 'members-edit'}) // 동등한 Children 계층이라면 parameter가 자동으로 넘어간다.
+      this.$router.push({path: `/members/${this.memberId}/edit`}) // 풀 path 입력 /members/members/{id}/edit
+      // this.$router.push({path: `${this.memberId}/edit`}) // 실제 등록한 path와 파라미터를 포함시켜야 이동가능하다.
     }
   }
 }
